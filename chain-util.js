8c2d7528c4ef0618e48aefd94bfc87aae2bbd667
Ndil-Adjim Sou Ngadoy <nasoungadoy@gmail.com>
2018-06-19 00:19:53 +0100

    extend wallet and transactions with signature

--- chain-util.js
+++ chain-util.js
@@ -1,6 +1,8 @@
 const EC = require("elliptic").ec;
+const SHA256 = require("crypto-js/sha256");
 const ec = new EC('secp256k1');
 const uuidV1 = require("uuid/v1");
+
 class ChainUtil {
     static genKeyPair() {
         return ec.genKeyPair();
@@ -8,6 +10,10 @@ class ChainUtil {
 
     static id() {
         return uuidV1();
+    }
+
+    static hash(data) {
+        return SHA256(JSON.stringify(data)).toString();
     }
 }
 
